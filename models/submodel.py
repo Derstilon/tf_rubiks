resolve problem with wrong dimensions

models/submodel.py
@@ -5,6 +5,8 @@ from enviroment.tensor_cube_env import TensorCubeEnv
 from solvers import build_basic_solver
 from scramblers import build_basic_scrambler
 import numpy as np
+
+
 class CubeGAN(Model):
     def __init__(self, scrambler, solver, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,48 +18,31 @@ class CubeGAN(Model):
         self.solver = solver
         self.cube_env = TensorCubeEnv()
 
-    def get_moves(self, moves):
-        result = []
-        for move in moves.numpy()[0]:
-            row_max = [index for index, item in enumerate(move) if item == max(move)]
-            result.append(row_max[0])
-        return result
-
     def train_step(self):
 
         self.cube_env.reset()
-
-        moves = self.scrambler(np.random.rand(1, 10))
-        self.cube_env.performMoves(self.get_moves(moves))
+        with tf.GradientTape() as scrambler_tape:
+            random_moves = self.scrambler(np.random.rand(1, 10))
+            self.cube_env.performMoves(tf.math.argmax(random_moves, 2).numpy()[0])
 
         tensor = self.cube_env.getTensor()
-        tensor = tf.reshape(tensor, (6, 3, 3))
+        tensor = tf.reshape(tensor, (1, 6, 3, 3))
 
-        # TODO ustalic shape'y
-        print(tensor)
+        with tf.GradientTape() as solver_tape:
+            solve_moves = self.solver(tensor)
+            self.cube_env.performMoves(tf.math.argmax(solve_moves, 2).numpy()[0])
 
-        result = self.solver(tensor) # TODO jaki result
-
-        # w zaleznosci od resulta albo
-
-        if result:
-            total_loss = self.cube_env.getLoss()
-        else:
-            self.cube_env.performMoves(moves)
-            total_loss = self.cube_env.getLoss()
-
-        # jeden model dostaje kare na podstawie lossa
-
-        # drugi dostaje nagrode
+        total_loss = self.cube_env.getLoss()
 
         # Apply backpropagation
-        scrambler_gradient = tf.GradientTape().gradient(total_loss,self.scrambler.trainable_variables)
-        solver_gradient = tf.GradientTape().gradient(1 - total_loss, self.solver.trainable_variables)
+        scrambler_gradient = scrambler_tape.gradient(1 - total_loss, self.scrambler.trainable_variables)
+        solver_gradient = solver_tape.gradient(total_loss, self.solver.trainable_variables)
+
         opt = tf.keras.optimizers.experimental.SGD(learning_rate=0.1)
         opt.apply_gradients(zip(scrambler_gradient, self.scrambler.trainable_variables))
         opt.apply_gradients(zip(solver_gradient, self.solver.trainable_variables))
 
-        return {"scrambler_loss": total_loss, "solver_loss": 1 - total_loss}
+        return {"scrambler_loss": 1 - total_loss, "solver_loss": total_loss}
 
 
 
@@ -66,5 +51,4 @@ if __name__ == "__main__":
     solver = build_basic_solver()
     scrambler = build_basic_scrambler()
     cubeGAN = CubeGAN(scrambler, solver)
-
     cubeGAN.train_step()